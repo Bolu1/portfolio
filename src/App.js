df5cbb12030f641dfec85dc872211ba4d9867ee6
Bolu Adetifa <boluadetifa@gmail.com>
2025-07-31 12:04:14 +0100

    feat(app): implement redirect to notion site

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import { AnimatePresence } from "framer-motion";
-import { lazy } from "react";
-import { Route, Routes, useLocation, useRoutes } from "react-router-dom";
-import SideLinks from "./global/SideLinks";
-import Spinner from "./global/Spinner";
-import About from "./Pages/About";
+import { useEffect } from "react";
 
-const Home = lazy(() => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(import("./Pages/Home")), 3000);
-  });
-});
-// const routes = [{ path: "", element: <Home /> }];
 function App() {
-  // const element = useRoutes(routes);
-  const location = useLocation();
+  useEffect(() => {
+    // Redirect immediately when the app loads
+    window.location.href = "https://boluadetifa.notion.site/Bolu-Software-Engineer-2378e468621e805fb21ecef1037f4db5";
+  }, []);
+
+  // Optional: Show a brief loading message before redirect
   return (
-    <div className="App bg-black">
-        <AnimatePresence mode="wait" initial={false}>
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-          </Routes>
-        </AnimatePresence>
-      {/* <SideLinks /> */}
+    <div className="flex items-center justify-center min-h-screen bg-black App">
+      <div className="text-center text-white">
+        <p>Redirecting...</p>
+      </div>
     </div>
   );
 }
